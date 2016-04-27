bfc8afd7899c6e7471d20e12124696455e58001b
Agnes Kosgei <agykosgei@gmail.com>
2016-04-27 10:24:40 +0300

    data type function

---
+++ day_3/data_type.py
@@ -0,0 +1,30 @@
+def data_type(x):
+	'''
+	Takes in an argument ,x:
+	-for a interger,return x**2
+	-for a float,return x/2
+	-for a string,returns"hello" + x
+	-for a boolean ,returns"boolean"
+	-for a  long,return squareroot(x)
+	'''
+	if type(x) == int:
+		return x ** 2
+	elif type(x) == float:
+		return x / 2
+	elif type(x) == str:
+		return "hello"+ x
+	elif type(x) == bool:
+		return "boolean"
+	elif type (x) == long:
+		return "long"
+	else:
+		return ()
+
+print data_type(1)
+print data_type(2.4)
+print data_type('hello')
+print data_type(True)
+print data_type(123456789098765432345678)
+
+
+
